8fe363b989d8a0cafd2e15ac2a2d2701dde95cc5
Amit Awasthi <amit.awasthi@lendingkart.com>
2026-07-29 00:18:28 +0530

    final commit

---
+++ src/helper.py
@@ -0,0 +1,47 @@
+from langchain.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from typing import List
+from langchain.schema import Document
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+
+
+
+#Extracting text from PDF files
+def load_pdf_files(data):
+    loader = DirectoryLoader(data, glob="**/*.pdf", loader_cls=PyPDFLoader)
+    documents = loader.load()
+    return documents
+
+
+def filter_to_minimal_docs(documents: List[Document]) -> List[Document]:
+    """
+    Filters the list of documents to only include those with minimal content.
+    A document is considered minimal if it contains only 'source' in the metadata  and the original page_content.
+    """
+    minimal_docs:List[Document] = []
+    for doc in documents:
+        src=doc.metadata.get("source")
+        minimal_docs.append(Document(page_content=doc.page_content, metadata={"source": src}))
+        
+    return minimal_docs
+
+
+#split the documents into smaller chunks
+
+def text_split(minimal_docs):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,
+    )
+    split_docs = text_splitter.split_documents(minimal_docs)
+    return split_docs
+
+
+def download_embeddings():
+    """
+    Downloads the HuggingFace embeddings model and returns an instance of HuggingFaceEmbeddings.
+    """
+    embeddings=HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    return embeddings
---
+++ src/prompt.py
@@ -0,0 +1,5 @@
+system_prompt = (
+    "You are a medical assistant.You will be provided with context from a set of documents.Use this context to answer the user's question.If the answer is not contained within the context,respond with 'I don't know.'. Use three sentences maximum and provide a concise and clear answer to the user's question."
+    "\n\n"
+    "{context}"
+)
